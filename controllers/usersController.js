4824c29e4841e58a0fd306225de4b8c2b5381cf6
petercolella <peter.joseph.colella@gmail.com>
2017-03-26 00:34:54 -0400

    user get show route

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -6,14 +6,26 @@ var User = require('../models/user');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   // res.send('respond with a resource');
-  User.find({})
-    .exec(function(err, users){
-      if (err) { console.log(err); }
-      console.log(users);
-      res.render('users/index.hbs', {
-        users: users
-      });
-    });
+  	User.find({})
+    	.exec(function(err, users){
+	    	if (err) { console.log(err); }
+	      	console.log(users);
+	      	res.render('users/index.hbs', {
+	        	users: users
+	      	});
+    	});
+});
+
+router.get('/:id', function(req, res) {
+    User.findById(req.params.id)
+        .exec(function(err, users) {
+            if(err) console.log(err);
+
+            console.log(users);
+            res.render('users/show.hbs', {
+        		users: users
+      		});
+        });
 });
 
 module.exports = router;
