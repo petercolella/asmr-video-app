6e7743cfd9047e1d72666b27ee63398e39ca7c71
petercolella <peter.joseph.colella@gmail.com>
2017-03-25 23:44:27 -0400

    user get index route and user index view

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var User = require('../models/user');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('respond with a resource');
+  User.find({})
+    .exec(function(err, users){
+      if (err) { console.log(err); }
+      console.log(users);
+      res.render('users/index.hbs', {
+        users: users
+      });
+    });
 });
 
 module.exports = router;
--- db/seeds.js
+++ db/seeds.js
@@ -31,8 +31,31 @@ var firstUser = new User({
   	}]
 });
 
+var secondUser = new User({
+    username: 'nunya',
+  	email: 'peter.joseph.colella@gmail.com',
+  	password_digest: '1234',
+  	about: 'None of your business.',
+  	video: [{
+  		artist_name: 'WhispersAudios ASMR',
+  		video_title: 'ASMR - Annual Physical Exam with Dr. Hastings',
+  		genre: 'Role Play',
+  		language: 'English',
+  		triggers: 'Soft Spoken',
+  		date: '04/24/2016',
+  		video_length_mins: '36',
+  		url: 'https://youtu.be/81H9B738C3g'
+  	}]
+});
+
 firstUser.save(function(err) {
   if (err) console.log(err);
 
   console.log('first user created!');
+});
+
+secondUser.save(function(err) {
+  if (err) console.log(err);
+
+  console.log('second user created!');
 });
