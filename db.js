daa3e70733ac1450c5bed2ea8218d197f274cabd
petercolella <peter.joseph.colella@gmail.com>
2017-03-28 21:13:24 -0400

    made changes to mongoose

--- db.js
+++ db.js
@@ -1,40 +1,24 @@
-var mongoose = require('mongoose');
+// var mongoose = require('mongoose');
 
-var db = mongoose.connection;
+// var db = mongoose.connection;
 
-mongoose.promise = global.Promise;
+// mongoose.promise = global.Promise;
 
-// CONNECTION EVENTS
-// Connect to database
-if (process.env.MONGODB_URI) {
-  mongoose.connect(process.env.MONGODB_URI);
-}
-else {
-  mongoose.connect('mongodb://localhost/asmr-video-app');
-}
-mongoose.connection.on('error', function(err) {
-  console.error('MongoDB connection error: ' + err);
-  process.exit(-1);
-  }
-);
-mongoose.connection.once('open', function() {
-  console.log("Mongoose has connected to MongoDB!");
-});
+// // CONNECTION EVENTS
+// db.once('open', function() {
+//   console.log("Opened mongoose.");
+// });
+// db.once('close', function() {
+//   console.log("Closed mongoose.");
+// });
+// db.on('connected', function() {
+//   console.log('Mongoose connected to ' + db.host + ':' + db.port + '/' + db.name);
+// });
+// db.on('error', function(err) {
+//   console.log('Mongoose connection error: ' + err);
+// });
+// db.on('disconnected', function() {
+//   console.log('Mongoose disconnected');
+// });
 
-db.once('open', function() {
-  console.log("Opened mongoose.");
-});
-db.once('close', function() {
-  console.log("Closed mongoose.");
-});
-db.on('connected', function() {
-  console.log('Mongoose connected to ' + db.host + ':' + db.port + '/' + db.name);
-});
-db.on('error', function(err) {
-  console.log('Mongoose connection error: ' + err);
-});
-db.on('disconnected', function() {
-  console.log('Mongoose disconnected');
-});
-
-module.exports = db;
+// module.exports = db;
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var session = require('express-session');
 var methodOverride = require('method-override');
 var livereload = require('connect-livereload');
 
-var db = require('./db');
+// var db = require('./db');
 
 // mongoose.connect('mongodb://localhost/asmr-video-app');
 
@@ -21,6 +21,22 @@ var videosController = require('./controllers/videosController');
 var sessionsController = require('./controllers/sessions');
 
 var app = express();
+
+// Connect to database
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+  mongoose.connect('mongodb://localhost/express-movies');
+}
+mongoose.connection.on('error', function(err) {
+  console.error('MongoDB connection error: ' + err);
+  process.exit(-1);
+  }
+);
+mongoose.connection.once('open', function() {
+  console.log("Mongoose has connected to MongoDB!");
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
