616f8721456c5e44edd949a1fcf579e9a7285f08
petercolella <peter.joseph.colella@gmail.com>
2017-03-29 11:53:07 -0400

    Use env config

--- server.js
+++ server.js
@@ -24,17 +24,17 @@ var sessionsController = require('./controllers/sessions');
 var app = express();
 
 // // Connect to database
-// mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI);
 
-// mongoose.connection.on('error', function(err) {
-//   console.error('MongoDB connection error: ' + err);
-//   process.exit(-1);
-//   }
-// );
-// mongoose.connection.once('open', function() {
-//   console.log(process.env.MONGODB_URI)
-//   console.log("Mongoose has connected to MongoDB!");
-// });
+mongoose.connection.on('error', function(err) {
+  console.error('MongoDB connection error: ' + err);
+  process.exit(-1);
+  }
+);
+mongoose.connection.once('open', function() {
+  console.log(process.env.MONGODB_URI)
+  console.log("Mongoose has connected to MongoDB!");
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -81,6 +81,6 @@ app.use(function(err, req, res, next) {
 
 module.exports = app;
 
-app.listen(5000, function(){
-  console.log("app listening on port 5000");
-});
+// app.listen(5000, function(){
+//   console.log("app listening on port 5000");
+// });
